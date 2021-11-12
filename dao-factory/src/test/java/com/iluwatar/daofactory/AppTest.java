0bfe9d0d111868932c2203dc8f9f32f6b62e8643
‘NatalieSo’ <nataliesotszyui@gmail.com>
2021-11-12 01:48:52 -0800

    Added UserTest and implemented some tests

--- dao-factory/src/test/java/com/iluwatar/daofactory/AppTest.java
+++ dao-factory/src/test/java/com/iluwatar/daofactory/AppTest.java
@@ -1,4 +1,15 @@
 package com.iluwatar.daofactory;
 
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class AppTest {
+
+    @Test
+    void shouldExecuteApplicationWithoutException() {
+        assertDoesNotThrow(() -> App.main(new String[]{}));
+    }
 }
---
+++ dao-factory/src/test/java/com/iluwatar/daofactory/DerbyUserDaoTest.java
@@ -0,0 +1,7 @@
+package com.iluwatar.daofactory;
+
+public class DerbyUserDaoTest {
+    private DerbyUserDAO dao;
+    private static final User user = new User();
+
+}
